2807e62a4b2bf9bbcad598fceb5485e132e0facc
Ermat <ealymbaev@gmail.com>
2022-10-06 17:34:14 +0600

    Organize and update source code

--- Package.swift
+++ Package.swift
@@ -3,25 +3,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "StorageKit",
-    defaultLocalization: "en",
-    platforms: [
-        .iOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "StorageKit",
-            targets: ["StorageKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/horizontalsystems/ThemeKit.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/horizontalsystems/ComponentKit.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", .upToNextMajor(from: "4.2.2")),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
-    ],
-    targets: [
-        .target(
-            name: "StorageKit",
-            dependencies: ["ThemeKit", "ComponentKit", "KeychainAccess", "SnapKit"]),
-    ]
+        name: "StorageKit",
+        defaultLocalization: "en",
+        platforms: [
+            .iOS(.v13),
+        ],
+        products: [
+            .library(
+                    name: "StorageKit",
+                    targets: ["StorageKit"]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", .upToNextMajor(from: "4.2.2")),
+            .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+            .package(url: "https://github.com/horizontalsystems/ComponentKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/horizontalsystems/ThemeKit.Swift.git", .upToNextMajor(from: "1.0.0")),
+        ],
+        targets: [
+            .target(
+                    name: "StorageKit",
+                    dependencies: [
+                        "KeychainAccess",
+                        "SnapKit",
+                        .product(name: "ComponentKit", package: "ComponentKit.Swift"),
+                        .product(name: "ThemeKit", package: "ThemeKit.Swift"),
+                    ]
+            ),
+        ]
 )
--- README.md
+++ README.md
@@ -1,25 +1,15 @@
-# Storage Kit
-
-
-## Requirements
+# StorageKit.Swift
 
 ## Installation
 
 ### Swift Package Manager
 
-The [Swift Package Manager](https://swift.org/package-manager/) is a tool for automating the distribution of Swift code
-and is integrated into the `swift` compiler. It is in early development, but StorageKit does support its use on
-supported platforms.
-
-Once you have your Swift package set up, adding StorageKit as a dependency is as easy as adding it to
-the `dependencies` value of your `Package.swift`.
-
 ```swift
 dependencies: [
-    .package(url: "https://github.com/horizontalsystems/StorageKit.git", .upToNextMajor(from: "1.0.0"))
+    .package(url: "https://github.com/horizontalsystems/StorageKit.Swift.git", .upToNextMajor(from: "1.0.0"))
 ]
 ```
 
 ## License
 
-StorageKit is available under the MIT license. See the LICENSE file for more info.
+`StorageKit.Swift` is available under the MIT license. See the LICENSE file for more info.
